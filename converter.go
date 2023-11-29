1eeedcf9540107c36bad15800f93671422b4b2a0
leaqved <leaqved+dev@gmail.com>
2023-11-29 18:16:00 +0300

    feat(converter): add "DefaultSplitter" function

--- converter.go
+++ converter.go
@@ -1,9 +1,39 @@
 package caseconv
 
-import "github.com/leaqved/splitter"
+import (
+	"unicode"
+
+	s "github.com/leaqved/splitter"
+)
+
+var DefaultSeparators = map[rune]struct{}{
+	' ': {},
+	'-': {},
+	'_': {},
+	'.': {},
+	',': {},
+	':': {},
+	';': {},
+}
 
 type Converter struct {
-	Splitter   *splitter.Splitter
+	Splitter   *s.Splitter
 	Delimeter  rune
 	Formatters []Format
+}
+
+func DefaultSplitter() *s.Splitter {
+	return s.New(
+		s.AddSkip(
+			s.Before(
+				s.CharInSet(DefaultSeparators))),
+		s.AddSplit(
+			s.After(unicode.IsLetter).And(
+				s.After(unicode.IsLower).And(
+					s.Before(unicode.IsLetter).And(
+						s.Before(unicode.IsUpper)))),
+		),
+		s.AddJoin(
+			s.Everything()),
+	)
 }
