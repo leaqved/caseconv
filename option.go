f756532c9341e9464d70d9ab285297f4d2220a62
leaqved <leaqved+dev@gmail.com>
2023-11-30 20:23:44 +0300

    feat(option): add "WithSplitter" function

--- option.go
+++ option.go
@@ -1,3 +1,11 @@
 package caseconv
 
+import "github.com/leaqved/splitter"
+
 type Option func(*Converter)
+
+func WithSplitter(splitter *splitter.Splitter) Option {
+	return func(c *Converter) {
+		c.Splitter = splitter
+	}
+}
