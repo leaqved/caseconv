2414bead54a916a5e2d44950aa40dc379809759e
leaqved <leaqved+dev@gmail.com>
2023-12-01 19:25:52 +0300

    feat(rule): add "WordIsFirst" function

--- rule.go
+++ rule.go
@@ -3,3 +3,9 @@ package caseconv
 type Rule func(index int, words []string) bool
 
 type CharRule func(index int, word string) bool
+
+func WordIsFirst() Rule {
+	return func(index int, words []string) bool {
+		return index == 0
+	}
+}
